0ba8e4915d7f31ac4150a5be9531c09eaa7f3535
openwebscada <openwebscada@gmail.com>
2009-11-19 22:14:21 +0000

    Test ExtJs visualization back-end GUI simple static example

---
+++ docs/extjs_view_admin_test/OpenWebScada_getNodes.php
@@ -0,0 +1,45 @@
+<?php
+echo '[{
+    "text": "Data bases",
+    "id": "station\/databases",
+    "cls": "folder"
+}, {
+    "text": "Security",
+    "id": "station\/security",
+    "cls": "folder"
+}, {
+    "text": "Transports",
+    "id": "station\/transports",
+    "leaf": true,
+    "cls": "file"
+}, {
+    "text": "Transport protocols",
+    "id": "station\/transport_protocols",
+    "leaf": true,
+    "cls": "file"
+}, {
+    "text": "Data acquisition",
+    "id": "station\/data_acquisition",
+    "leaf": true,
+    "cls": "file"
+}, {
+    "text": "Archives",
+    "id": "station\/archives",
+    "leaf": true,
+    "cls": "file"
+}, {
+    "text": "Specials",
+    "id": "station\/specials",
+    "leaf": true,
+    "cls": "file"
+}, {
+    "text": "User interfaces",
+    "id": "station\/user_interfaces",
+    "leaf": true,
+    "cls": "file"
+}, {
+    "text": "Modules scheduler",
+    "id": "station\/modules_scheduler",
+    "leaf": true,
+    "cls": "file"
+}]';
